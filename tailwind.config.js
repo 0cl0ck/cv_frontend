15557d94ac7a351e4e706961afa8e618413c23cf
0cl0ck <hugodewas@gmail.com>
2025-04-11 00:29:45 +0200

    chore: préparer l'upgrade de Tailwind CSS et DaisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,24 +18,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require('daisyui')],
-  daisyui: {
-    themes: [
-      {
-        light: {
-          "primary": "#126E62",          /* cv-green */
-          "secondary": "#002D4C",        /* cv-blue */
-          "accent": "#37CDBE",
-          "neutral": "#3D4451",
-          "base-100": "#FFFFFF",
-          "info": "#3ABFF8",
-          "success": "#36D399",
-          "warning": "#FBBD23",
-          "error": "#F87272"
-        },
-      },
-      "light",
-      "dark"
-    ],
-  },
+  plugins: [],
 }
