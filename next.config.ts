7971d28a2bd754a981bf3233c713f05d3553cb0b
0cl0ck <hugodewas@gmail.com>
2025-04-10 23:25:55 +0200

    fix: correction de la configuration Next.js PWA et ajout d'un script dev sans turbopack

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,18 @@
-import withPWA from 'next-pwa'
 import type { NextConfig } from 'next'
+import withPWA from 'next-pwa'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   // experimental: {
   //   serverActions: true, // si tu utilises les server actions
   // },
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
-  },
 }
 
-export default withPWA(nextConfig)
+const withPWAConfig = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+})
+
+export default withPWAConfig(nextConfig)
