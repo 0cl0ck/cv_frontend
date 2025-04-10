f46830f57683f49d327b7d9ea93e4d305dbb3e88
0cl0ck <hugodewas@gmail.com>
2025-04-10 18:28:33 +0200

    Add Aceternity UI Navbar component

---
+++ src/app/demo/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { NavbarWithChildren } from "@/components/ui/navbar";
+
+export default function DemoPage() {
+  return (
+    <main className="min-h-screen">
+      <NavbarWithChildren />
+    </main>
+  );
+}
