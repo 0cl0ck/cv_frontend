a18be90aa187f81c5bb3be938f4586874c04c075
0cl0ck <hugodewas@gmail.com>
2026-03-05 10:49:17 +0100

    feat: add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
@@ -108,6 +109,7 @@ export default async function RootLayout({
             </main>
             <Footer />
             <ClientLayoutWrapper />
+            <Analytics />
           </CartProvider>
         </AuthProvider>
       </body>
